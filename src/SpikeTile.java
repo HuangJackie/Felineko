ee8ad9a6fe1a283620d11ecb806270020dd6763a
Jackie Huang <jackie.huang@mail.utoronto.ca>
2018-12-26 22:27:32 -0500

    Added the Tile subclass SpikeTile, and its picture.

---
+++ src/SpikeTile.java
@@ -0,0 +1,13 @@
+public class SpikeTile extends Tile {
+
+    /**
+     * Creates a Tile with a type "name" and a X/Y location.
+     *
+     * @param type Type of tile/Name of the sprite png.
+     * @param x    X-coordinate of the tile.
+     * @param y    Y-coordinate of the tile.
+     */
+    SpikeTile(String type, int x, int y) {
+        super(type, x, y);
+    }
+}
